6ca3c1d01a90fb564a0c7d49da591aa7d49231a4
wyfsxs <wyfsxs@126.com>
2019-05-24 00:22:50 +0800

    commit

---
+++ message-thrift-python-service/message/api/message_service.py
@@ -0,0 +1,46 @@
+from message.api import MessageService
+from thrift.transport import TSocket
+from thrift.transport import TTransport
+from thrift.protocol import TBinaryProtocol
+from thrift.server import TServer
+import smtplib
+from email.mime.text import MIMEText
+from email.header import Header
+
+send = 'imoocd@163.com'
+authCode = 'aA111111'
+
+
+class MessageServiceHandler:
+    def sendMobileMessage(self, mobile, message):
+        print("sendMobileMessage, mobile" + mobile + ", message" + message)
+        return True
+
+    def sendEmailMessage(self, email, message):
+        print("sendEmailMessage, email:" + email + ", message:" + message)
+        messageObj = MIMEText(message, "planin", "uft-8")
+        messageObj['From'] = send
+        messageObj['To'] = email
+        messageObj['Subject'] = Header('慕课网邮件', 'utf-8')
+        try:
+           smtpObj = smtplib.SMTP('smtp.163.com')
+           smtpObj.login(send, authCode)
+           smtpObj.sendmail(send, [email], messageObj.as_string())
+           print("send mail success")
+           return True
+        except smtplib.SMTPException as ex:
+            print("send mail fail")
+            print(ex)
+            return False
+
+
+if __name__ == '__main__':
+    handle = MessageServiceHandler()
+    processor = MessageService.Processor(handle)
+    transport = TSocket.TServerSocket("localhost", "9090")
+    tfactory = TTransport.TFramedTransportFactory()
+    pfactory = TBinaryProtocol.TBinaryProtocolFactory
+    server = TServer.TSimpleServer(processor, transport, tfactory, pfactory)
+    print("python thrift server start")
+    server.serve()
+    print("python thrift server exit")
